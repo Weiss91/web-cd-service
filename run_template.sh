5d2ad1de27d7f5f99e5f7e16099f010fdf767c2e
MatthiasWeiss <matthias_weiss91@gmx.de>
2021-05-02 19:16:03 +0200

    add convenience bazel run script for local testing

---
+++ run_template.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# update build files
+bazelisk run //:gazelle
+
+# update build deps
+bazelisk run //:gazelle -- update-repos -from_file=go.mod -to_macro=deps.bzl%go_dependencies
+
+# set needed envs to start
+export GIT_USER="user"
+export GIT_PASSWORD="pw"
+export GIT_DATA_PATH="sample/path"
+export REGISTRY="registry"
+export REGISTRY_USER="registry_user"
+export REGISTRY_PASSWORD="registry_password"
+export DOCKER_CONF_PATH="./config.json"
+
+bazelisk.exe run bazelwebapi:bazelwebapi
